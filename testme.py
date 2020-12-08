2f1c2fb1fec2e69ce117ccb139af5d30fae9d963
Jen Woolfe <jwoolfe@MacBook-Pro.local>
2020-12-08 11:42:54 -0800

    More work with selenium

--- testme.py
+++ testme.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 # Selenium Tutorial #1
 # 
+
+# Test ChromeDriver
 from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://nytimes.com")
 headlines = driver.find_elements_by_class_name("story-heading")
+
+# Test GeckoDriver
+driver = webdriver.Firefox()
+driver.get('https://www.seattletimes.com')
+headlines = driver.find_elements_by_class_name('story-heading')
